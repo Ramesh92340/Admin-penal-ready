c0b00dbbba39e89807363699d14fdc192eb0b182
Ramesh <rameshpilli1428@gmail.com>
2024-06-07 16:30:41 +0530

    date base added

--- admin/public/login.php
+++ admin/public/login.php
@@ -1,5 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
+<!DOCTYPE php>
+<php lang="en">
 
 <head>
 
@@ -41,40 +41,50 @@
                                     <div class="text-center">
                                         <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
                                     </div>
-                                    <form class="user">
+ 
+
+
+
+                                    <form class="user"  action="../includes/login_handler.php" method="POST">
                                         <div class="form-group">
-                                            <input type="email" class="form-control form-control-user"
-                                                id="exampleInputEmail" aria-describedby="emailHelp"
-                                                placeholder="Enter Email Address...">
+                                             
+                                            <input type="text" class="form-control form-control-user"
+                                            id="username" name="username"  
+                                                placeholder="Enter username...">
                                         </div>
                                         <div class="form-group">
+                                           
+
                                             <input type="password" class="form-control form-control-user"
-                                                id="exampleInputPassword" placeholder="Password">
+                                            id="password" name="password" placeholder="Password">
                                         </div>
-                                        <div class="form-group">
+
+                                        <!-- <div class="form-group">
                                             <div class="custom-control custom-checkbox small">
                                                 <input type="checkbox" class="custom-control-input" id="customCheck">
                                                 <label class="custom-control-label" for="customCheck">Remember
                                                     Me</label>
                                             </div>
-                                        </div>
-                                        <a href="index.html" class="btn btn-primary btn-user btn-block">
-                                            Login
+                                        </div> -->
+                                        <a href="index.php">
+                                        <button type="submit"  class="btn btn-primary btn-user btn-block">Login</button>
                                         </a>
-                                        <hr>
-                                        <a href="index.html" class="btn btn-google btn-user btn-block">
-                                            <i class="fab fa-google fa-fw"></i> Login with Google
-                                        </a>
-                                        <a href="index.html" class="btn btn-facebook btn-user btn-block">
-                                            <i class="fab fa-facebook-f fa-fw"></i> Login with Facebook
-                                        </a>
+                                         
                                     </form>
+
+
+
+
+                                    <?php
+            if (isset($_GET['error'])) {
+                echo "<p style='color:red;'>".htmlspecialchars($_GET['error'])."</p>";
+            }
+        ?>
+
                                     <hr>
+                                    
                                     <div class="text-center">
-                                        <a class="small" href="forgot-password.html">Forgot Password?</a>
-                                    </div>
-                                    <div class="text-center">
-                                        <a class="small" href="register.html">Create an Account!</a>
+                                        <a class="small" href="register.php">Create an Account!</a>
                                     </div>
                                 </div>
                             </div>
@@ -88,6 +98,10 @@
 
     </div>
 
+
+
+
+
     <!-- Bootstrap core JavaScript-->
     <script src="vendor/jquery/jquery.min.js"></script>
     <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
@@ -100,4 +114,4 @@
 
 </body>
 
-</html>
+</php>
